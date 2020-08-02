242bdf8f5556f67e893ee93e6883df393e86b9ae
bendevera <ben10devera@gmail.com>
2020-08-01 17:57:40 -0700

    making filenames more intuitive

---
+++ test_client.py
@@ -0,0 +1,12 @@
+import os
+
+from client import GithubClient
+
+
+def test_db() -> None:
+    TEST_OWNER = "brendajin"
+    TEST_REPO = "bendevera"
+    GITHUB_TOKEN = str(os.getenv("GITHUB_TOKEN"))
+    client = GithubClient(GITHUB_TOKEN, TEST_OWNER, TEST_REPO)
+    pull_requests = client.list_pull_requests()
+    assert len(pull_requests) == 1
